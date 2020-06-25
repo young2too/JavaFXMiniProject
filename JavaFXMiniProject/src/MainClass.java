e200ea254bc1646ae7bfb1384834157c6b93cd0b
young2too <young2too2@naver.com>
2020-06-25 23:33:24 +0900

    BuildPath수정

--- JavaFXMiniProject/src/MainClass.java
+++ JavaFXMiniProject/src/MainClass.java
@@ -1,6 +1,15 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
 
-public class MainClass {
+public class MainClass extends Application{
 	public static void main(String[] args) {
 		System.out.println("미니 프로젝트 GIT입니다");
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+		
 	}
 }
