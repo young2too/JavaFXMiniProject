eaf75d95a7684dcc96d3eeac2dd27fa8a3611d56
BlakeKim <59683657+BlakeKim@users.noreply.github.com>
2020-06-29 16:02:23 +0900

    비밀번호찾기0629
    비밀번호찾기0629

--- JavaFXMiniProject/src/PwOk/PwOkService.java
+++ JavaFXMiniProject/src/PwOk/PwOkService.java
@@ -5,27 +5,20 @@ import java.util.List;
 import javafx.scene.Parent;
 
 public interface PwOkService {
+	public boolean compareId(String id, String idOk);
+	// id,idOk 일치여부 확인
 	
-	public Parent OpenMembershipForm();
-	public boolean compareNewPW(String newPw, String newPwOk);
-	/*
-	 *  compareNewpW -새로운 비밀번호 비교 메소드
-	 *  newPw: 새로운 비밀번호 필드 
-	 *  newPwOk: 새로운 비밀번호 확인 필드
-	 */
-	public boolean compareQuiz(String quiz, String quizOk);;
-	/*
-	 *  compareQuiz -질문 비교 메소드
-	 *  quiz: 질문 필드(회원가입에서 입력할 질문) 
-	 *  quizOk: 질문 확인 필드(비밀번호찾기화면에서 선택할 질문)
-	 */
-	public boolean compareAnswer(String answer, String answerOk);
-	/*
-	 *  compareAnswer -질문답 비교 메소드
-	 *  answer: 질문답 필드(회원가입에서 입력할 질문) 
-	 *  answerOk: 질문답 확인 필드(비밀번호찾기화면에서 선택할 질문)
-	 */
-	public boolean isComboBox(Parent membershipForm);
-	public String getComboBoxString(Parent membershipForm);
-	public void AddComboBox(Parent form, List<String> items);
+	public void checkId(Parent pwOkForm); 
+	// 
+	
+	public void checkQuiz(Parent pwOkForm);
+	public void setCheckBtn(Parent pwOkForm);
+	public void registerNewPw(Parent pwOkForm);
+	public boolean comparePw(Parent pwOkform);
+
+
+	
+	public boolean isComboBox(Parent pwOkform);
+	public String getComboBoxString(Parent pwOkform);
+	public void AddComboBox(Parent pwOkform);
 }
--- JavaFXMiniProject/src/PwOk/PwOkServiceImpl.java
+++ JavaFXMiniProject/src/PwOk/PwOkServiceImpl.java
@@ -1,60 +1,127 @@
 package PwOk;
 
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
 import CommonService.CommonService;
 import CommonService.CommonServiceImpl;
+import DataBase.DataBaseService;
+import DataBase.DataBaseServiceImpl;
+import DataBase.Member;
 import javafx.scene.Parent;
+import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
 import javafx.stage.Stage;
+import oracle.jdbc.driver.DBConversion;
 
 public class PwOkServiceImpl implements PwOkService {
-
+	
+	CommonService comSrv = new CommonServiceImpl();
+	PwOkServiceImpl pwSrv;
+	Member member;
+	DataBaseService db = new DataBaseServiceImpl();
+	
 	@Override
-	public boolean compareNewPW(String newPw, String newPwOk) {
+	public boolean compareId(String id, String idOk) {
 		// TODO Auto-generated method stub
-		newPw = "";
-		newPwOk = "";
-		return newPw.contentEquals(newPwOk);
+		id = member.getId();
+		
+		return id.equals(idOk);
 	}
 
 	@Override
-	public boolean compareQuiz(String quiz, String quizOk) {
+	public void checkId(Parent pwOkForm) {
 		// TODO Auto-generated method stub
-		return quiz.contentEquals(quizOk);
-	}
-
-	@Override
-	public boolean compareAnswer(String answer, String answerOk) {
-		// TODO Auto-generated method stub
-		return answer.contentEquals(answerOk);
-	}
-
-	@Override
-	public boolean isComboBox(Parent root) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public Parent OpenMembershipForm() {
-		// TODO Auto-generated method stub
-		CommonService comSrv = new CommonServiceImpl();
-		Stage membershipForm = new Stage();
-		Parent form = comSrv.showWindow(membershipForm, "/PwOk/PwOkScene.fxml");
-
-		String[] items = {"어머니 성함은?", "아버지 성함은?", "보물 1호는?"};
-		PwOkService pwOkSrv = new PwOkServiceImpl();
-		pwOkSrv.AddComboBox(form, Arrays.asList(items));
-
-		return form;
+		TextField idTxt = (TextField)pwOkForm.lookup("#idTxt");
+		
 	}
 	
 	@Override
-	public String getComboBoxString(Parent membershipForm) {
+	public void checkQuiz(Parent pwOkForm) {
 		// TODO Auto-generated method stub
-		ComboBox<String> cmbQuiz = (ComboBox<String>) membershipForm.lookup("#cmbQuiz");
+		TextField cmbQuiz = (TextField)pwOkForm.lookup("#cmbQuiz");
+		
+	}
+
+	@Override
+	public void setCheckBtn(Parent pwOkForm) {
+		// TODO Auto-generated method stub
+		Button confirmIdBtn = (Button)pwOkForm.lookup("#confirmIdBtn");
+		Button confirmQuizBtn = (Button)pwOkForm.lookup("#confirmQuizBtn");
+		Button confirmPwBtn = (Button)pwOkForm.lookup("#confirmPwBtn");
+		
+		confirmIdBtn.setOnMouseClicked(e->{
+			comSrv.setMouserBtnCursurEffect(confirmIdBtn);
+		});
+		
+		confirmQuizBtn.setOnMouseClicked(e->{
+			TextField newPwTxt = (TextField)pwOkForm.lookup("#newPwTxt");
+			TextField newPwOkTxt = (TextField)pwOkForm.lookup("#newPwOkTxt");
+			
+			newPwTxt.setDisable(!newPwTxt.isDisable());
+			newPwOkTxt.setDisable(!newPwOkTxt.isDisable());
+			confirmPwBtn.setDisable(!confirmPwBtn.isDisable());
+			comSrv.setMouserBtnCursurEffect(confirmIdBtn);
+		});
+	}
+
+	@Override
+	public void registerNewPw(Parent pwOkForm) {
+		// TODO Auto-generated method stub
+		String[] txtFldArr= {"#newPwTxt", "#newPwOkTxt"};
+		Map<String, TextField> txtFldMap = comSrv.getTextFieldInfo(pwOkForm, txtFldArr);
+		if(comSrv.isEmpty(txtFldMap, txtFldArr)) {
+			comSrv.alertWindow("오류 발생", "작성을 완료해주세요", AlertType.ERROR);
+			return;
+		}
+		else if(comparePw(pwOkForm)) {
+			System.out.println(comparePw(pwOkForm));
+			comSrv.alertWindow("오류 발생", "비밀번호가 일치하지 않습니다", AlertType.ERROR);
+			return;
+		}
+		System.out.println("다 입력했으니 DB에 등록하면 됨");
+	}
+
+	@Override
+	public boolean comparePw(Parent pwOkform) {
+		// TODO Auto-generated method stub
+		TextField newPwTxt = (TextField)pwOkform.lookup("#newPwTxt");
+		TextField newPwOkTxt = (TextField)pwOkform.lookup("#newPwOkTxt");
+		
+		System.out.println("oldPw를 DB에서 찾아와서 비교하는 곳");
+		
+		if(newPwTxt.getText().equals(newPwOkTxt.getText())) {
+			return false;
+		}
+		else
+			return true;
+	}
+
+	@Override
+	public boolean isComboBox(Parent pwOkform) {
+		// TODO Auto-generated method stub
+		CommonService comSrv = new CommonServiceImpl();
+		ComboBox<String> cmbQuiz = (ComboBox<String>)pwOkform.lookup("#cmbQuiz");
+		if(cmbQuiz == null) {
+			comSrv.alertWindow("오류 발생", "콤보박스가 비어 있습니다.", AlertType.ERROR);
+			return false;
+		} else if (cmbQuiz.getValue() == null) {
+			comSrv.alertWindow("오류 발생", "콤보박스가 비어 있습니다.", AlertType.ERROR);
+			return false;
+		}
+		return true;
+	}
+
+	@Override
+	public String getComboBoxString(Parent pwOkform) {
+		// TODO Auto-generated method stub
+		ComboBox<String> cmbQuiz = (ComboBox<String>) pwOkform.lookup("#cmbQuiz");
 
 		if (cmbQuiz == null) {
 			return "";
@@ -63,10 +130,15 @@ public class PwOkServiceImpl implements PwOkService {
 	}
 
 	@Override
-	public void AddComboBox(Parent form, List<String> items) {
+	public void AddComboBox(Parent pwOkform) {
 		// TODO Auto-generated method stub
-		ComboBox<String> cmbQuiz = (ComboBox<String>) form.lookup("#cmbQuiz");
-
+		ComboBox<String> cmbQuiz = (ComboBox<String>)pwOkform.lookup("#cmbQuiz");
+		
+		List<String> items = new ArrayList<String>();
+		items.add("어머니 성함은?");
+		items.add("아버지 성함은?");
+		items.add("보물 1호는?");
+		
 		if (cmbQuiz != null) {
 			for (String item : items) {
 				cmbQuiz.getItems().add(item);
@@ -74,5 +146,8 @@ public class PwOkServiceImpl implements PwOkService {
 		}
 
 	}
+	
+
+
 
 }
