ebf7f5ee2142a15c7299ab9648bacde6e578f17d
young2too <young2too2@naver.com>
2020-06-30 15:49:55 +0900

    PwOk리커밋

--- JavaFXMiniProject/src/PwOk/PwOkController.java
+++ JavaFXMiniProject/src/PwOk/PwOkController.java
@@ -1,62 +1,68 @@
 package PwOk;
 
 import java.net.URL;
+import java.sql.SQLException;
 import java.util.Map;
 import java.util.ResourceBundle;
-
 import CommonService.CommonService;
 import CommonService.CommonServiceImpl;
 import CommonService.Controller;
+import DataBase.DataBaseService;
+import DataBase.DataBaseServiceImpl;
+import DataBase.Member;
 import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
 import javafx.scene.AccessibleAction;
 import javafx.scene.Parent;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
 
 public class PwOkController extends Controller implements Initializable {
-	private Parent root;
-	private CommonService comSrv;
-	private PwOkService pwOkSrv;
+	Parent pwOkForm;
+	CommonService comSrv;
+	PwOkService pwOkSrv;
+	Member member;
+	DataBaseService dbSrv;
 
+	Button backBtn;
+
+	@Override
 	public void setRoot(Parent root) {
-		this.root = root;
+		// TODO Auto-generated method stub
+		this.pwOkForm = root;
+		backBtn = (Button) pwOkForm.lookup("#backImgBtn");
+		comSrv.setMouserBtnCursurEffect(backBtn);
+		pwOkSrv.setCheckBtn(pwOkForm);
+		pwOkSrv.AddComboBox(pwOkForm);
 	}
 
+	public void cancelBtnAction() {
+		comSrv.WindowClose(pwOkForm);
+		comSrv.showWindow(new Stage(), "/MyInfo/MyInfo.fxml");
+	}
+	
+	public void confirmIdBtnAction() {
+		pwOkSrv.checkId(pwOkForm);
+	}
+
+	public void confirmQuizBtnAction() {
+		pwOkSrv.checkQuiz(pwOkForm);
+	}
+	
+	public void confirmPwBtnAction() {
+		pwOkSrv.registerNewPw(pwOkForm);
+		comSrv.showWindow(new Stage(), "/MyInfo/MyInfo.fxml");
+	}
+	
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		// TODO Auto-generated method stub
 		comSrv = new CommonServiceImpl();
-		pwOkSrv = new PwOkServiceImpl();;
+		pwOkSrv = new PwOkServiceImpl();
+		dbSrv = new DataBaseServiceImpl();
 	}
 
-	public void CancelProc(ActionEvent event) {
-		comSrv.WindowClose(event);
-	}
-
-	private boolean isCheck(Map<String, TextField> txtFldMap, String[] txtFldArr, String[] list) {
-		if (comSrv.isEmpty(txtFldMap, txtFldArr, list)) {
-			System.out.println("비어 있습니다.");
-			return false;
-		}
-
-		String pw = txtFldMap.get(txtFldArr[2]).getText();
-		String pwOk = txtFldMap.get(txtFldArr[3]).getText();
-
-		if (!pwOkSrv.compareNewPW(pw, pw)) {
-			comSrv.alertWindow("오류", "패스워드가 다릅니다.", AlertType.INFORMATION);
-			txtFldMap.get(txtFldArr[2]).clear();
-			txtFldMap.get(txtFldArr[2]).requestFocus();
-			txtFldMap.get(txtFldArr[3]).clear();
-			System.out.println("패스워드가 다릅니다.");
-			return false;
-		}
-
-		if (!pwOkSrv.isComboBox(root)) {
-			return false;
-		}
-
-		return true;
-	}
-
+	
 }
